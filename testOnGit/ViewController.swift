898da95e3cbc3ac3d685499c30867b0ee06b49a9
ios假日班 <ios@mac-12.local>
2018-02-07 20:20:05 +0800

    Initial Commit

---
+++ testOnGit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testOnGit
+//
+//  Created by ios on 2018/2/7.
+//  Copyright © 2018年 sarahwu2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var personNames:[String] = [];
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
