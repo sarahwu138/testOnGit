747e372c918d9e7b2d0549a689714b0b6e327908
ios假日班 <ios@mac-12.local>
2018-02-07 20:38:48 +0800

    add cc variable on page of AppDelegate

--- testOnGit/AppDelegate.swift
+++ testOnGit/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var doremimi:String? = nil;
+    var cc:String?;
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
